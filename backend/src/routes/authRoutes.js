082500d98bf152867797698a47d971882fef2715
Nelnel03 <nelsonaraya0364@gmail.com>
2026-05-13 15:49:28 -0600

    implementacion una capa de protección contra ataques de fuerza bruta y denegación de servicio (DoS) utilizando una estrategia de Rate Limiting (limitación de tasa)

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authCrud = require('../cruds/authCrud');
 const { body, validationResult } = require('express-validator');
+const { authLimiter } = require('../middlewares/rateLimitMiddleware');
 
 /**
  * Middleware para manejar errores de validación
@@ -17,14 +18,14 @@ const validate = (req, res, next) => {
 // --- Rutas Públicas ---
 
 // POST: Login
-router.post('/login', [
+router.post('/login', authLimiter, [
     body('email').isEmail().withMessage('Email no válido'),
     body('password').notEmpty().withMessage('La contraseña es obligatoria'),
     validate
 ], authCrud.login);
 
 // POST: Registro
-router.post('/register', [
+router.post('/register', authLimiter, [
     body('nombre').notEmpty().withMessage('El nombre es obligatorio'),
     body('email').isEmail().withMessage('Email no válido'),
     body('password').isLength({ min: 6 }).withMessage('Mínimo 6 caracteres'),
